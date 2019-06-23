d9503f890901e35201ba275e9da5ca90c006e5dc
Cowary <42502964+Cowary@users.noreply.github.com>
2019-06-23 17:37:44 +0300

    Add files via upload

---
+++ RemoteYandex.Music/src/ru/covary/Main.java
@@ -0,0 +1,88 @@
+package ru.covary;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Main {
+
+    ServerSocket ss;
+    Socket socket;
+    InputStream in;
+    DataInputStream inData;
+    int port;
+    String code;
+    InetAddress thisIP;
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        while (true){
+            m.startServer();
+            try {
+                m.socket.close();
+                m.ss.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+    }
+    public void startServer() {
+        port = 9056;
+        try {
+            thisIP = InetAddress.getLocalHost();
+            System.out.println("IP: " + thisIP.getHostAddress());
+            ss = new ServerSocket(port);
+            socket = ss.accept();
+            System.out.println("Клиент подклчен.");
+            in = socket.getInputStream();
+            inData = new DataInputStream(in);
+
+            while (true) {
+                code = inData.readUTF().trim();
+                System.out.println(code);
+                switch (code) {
+                    case "CodePlayStopTreck":
+                        pressKey(KeyEvent.VK_CONTROL, KeyEvent.VK_P);
+                        break;
+                    case "CodePreviousTreck":
+                        pressKey(KeyEvent.VK_CONTROL, KeyEvent.VK_B);
+                        break;
+                    case "CodeNextTreck":
+                        pressKey(KeyEvent.VK_CONTROL, KeyEvent.VK_F);
+                        break;
+                    case "CodeRepeatTreck":
+                        pressKey(KeyEvent.VK_CONTROL, KeyEvent.VK_T);
+                        break;
+                    case "CodeRandomTreck":
+                        pressKey(KeyEvent.VK_CONTROL, KeyEvent.VK_H);
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+        public void pressKey(int keven_1, int keven_2){
+        Robot r = null;
+        try {
+            r = new Robot();
+            r.keyPress(keven_1);
+            r.keyPress(keven_2);
+            r.keyRelease(keven_2);
+            r.keyRelease(keven_1);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
