5dfe8dfd6018cf755ae2ccaa458721debe02c38a
Aleksandr Trishanov <atrishanov@tsconsulting.com>
2020-09-07 15:42:33 +0300

    KEK

---
+++ RemoteYandex.Music/src/ru/covary/Test.java
@@ -0,0 +1,11 @@
+package ru.covary;
+
+public class Test {
+    public static void main(String[] args) {
+        printlk();
+    }
+
+    public static void printlk(){
+        System.out.println("Kek");
+    }
+}
